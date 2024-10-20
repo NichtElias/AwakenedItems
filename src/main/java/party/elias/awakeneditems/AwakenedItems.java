21397ece3ef86f812a5b27d91415612459523080
NichtElias <eliasnie@t-online.de>
2024-10-20 22:42:57 +0200

    cleaned up example stuff

--- src/main/java/party/elias/awakeneditems/AwakenedItems.java
+++ src/main/java/party/elias/awakeneditems/AwakenedItems.java
@@ -1,7 +1,6 @@
 package party.elias.awakeneditems;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
@@ -30,15 +29,14 @@ import java.util.function.Supplier;
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(AwakenedItems.MODID)
 public class AwakenedItems {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "awakeneditems";
-    // Directly reference a slf4j logger
+
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "awakeneditems" namespace
+
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "awakeneditems" namespace
+
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "awakeneditems" namespace
+
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
     public static final DeferredRegister.DataComponents DATA_COMPONENTS = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, MODID);
@@ -47,9 +45,8 @@ public class AwakenedItems {
 
     public static final DeferredItem<Item> SOULSTUFF_ITEM = ITEMS.registerSimpleItem("soulstuff", new Item.Properties());
 
-    // Creates a creative tab with the id "awakeneditems:example_tab" for the example item, that is placed after the combat tab
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> AI_TAB = CREATIVE_MODE_TABS.register("awakeneditems_tab", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.awakeneditems")).withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> SOULSTUFF_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
-        output.accept(SOULSTUFF_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+        output.accept(SOULSTUFF_ITEM.get());
     }).build());
 
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<AwakenedItemData>> AWAKENED_ITEM_COMPONENT = DATA_COMPONENTS.registerComponentType("awakened_item",
@@ -58,8 +55,6 @@ public class AwakenedItems {
     public static final Supplier<AttachmentType<AwakenedItemPlayerData>> AWAKENED_ITEM_PLAYER_DATA_ATTACHMENT = ATTACHMENT_TYPES.register("awakened_item_player_data",
             () -> AttachmentType.builder(() -> new AwakenedItemPlayerData(0)).serialize(AwakenedItemPlayerData.CODEC).copyOnDeath().build());
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public AwakenedItems(IEventBus modEventBus, ModContainer modContainer) {
 
         BLOCKS.register(modEventBus);
@@ -69,14 +64,11 @@ public class AwakenedItems {
         ATTACHMENT_TYPES.register(modEventBus);
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+
         }
     }
 
@@ -84,8 +76,7 @@ public class AwakenedItems {
     public static class CommonModEvents {
         @SubscribeEvent
         public static void commonSetup(final FMLCommonSetupEvent event) {
-            // Some common setup code
-            LOGGER.info("HELLO FROM COMMON SETUP");
+
         }
 
         @SubscribeEvent
