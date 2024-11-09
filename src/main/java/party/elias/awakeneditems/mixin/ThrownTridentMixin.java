0f05b3916ecdf89a119454f9a6c15242c142f0d7
NichtElias <eliasnie@t-online.de>
2024-11-09 22:58:05 +0100

    thrown trident damage bonus

---
+++ src/main/java/party/elias/awakeneditems/mixin/ThrownTridentMixin.java
@@ -0,0 +1,30 @@
+package party.elias.awakeneditems.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.projectile.ThrownTrident;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.phys.EntityHitResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import party.elias.awakeneditems.AwakenedItemData;
+import party.elias.awakeneditems.AwakenedItems;
+
+@Mixin(ThrownTrident.class)
+public class ThrownTridentMixin {
+
+    @ModifyVariable(method = "onHitEntity", name = "f", at = @At(value = "STORE", ordinal = 0))
+    private float modifyDamage(float value, EntityHitResult result) {
+        ThrownTrident trident = ((ThrownTrident)(Object)this);
+        Entity thrower = trident.getOwner();
+        ItemStack itemStack = trident.getWeaponItem();
+
+        AwakenedItemData awakenedItemData = itemStack.get(AwakenedItems.AWAKENED_ITEM_COMPONENT);
+
+        if (awakenedItemData != null && awakenedItemData.owner().equals(thrower.getUUID())) {
+            value += (float) (awakenedItemData.level() / 20.0 * 12.0);
+        }
+
+        return value;
+    }
+}
