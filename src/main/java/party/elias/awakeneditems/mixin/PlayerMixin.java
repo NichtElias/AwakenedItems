efbd808202c1542b42acc48914d93ffe81fe13de
NichtElias <eliasnie@t-online.de>
2024-11-12 00:28:37 +0100

    awakened shields have a chance to not get disabled by axes

---
+++ src/main/java/party/elias/awakeneditems/mixin/PlayerMixin.java
@@ -0,0 +1,27 @@
+package party.elias.awakeneditems.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import party.elias.awakeneditems.AwakenedItemData;
+import party.elias.awakeneditems.AwakenedItems;
+
+@Mixin(Player.class)
+public class PlayerMixin {
+    @WrapOperation(method = "blockUsingShield", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;disableShield()V"))
+    private void blockUsingShield(Player instance, Operation<Void> original) {
+        ItemStack itemStack = instance.getUseItem();
+        AwakenedItemData awakenedItemData = itemStack.get(AwakenedItems.AWAKENED_ITEM_COMPONENT);
+
+        if (awakenedItemData != null) {
+            if (instance.getRandom().nextDouble() < awakenedItemData.level() / 20.0) {
+                return;
+            }
+        }
+
+        original.call(instance);
+    }
+}
